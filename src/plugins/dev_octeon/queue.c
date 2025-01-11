81e8be1028c0a49f1390062634a327127c3bd3ca
Monendra Singh Kushwaha <kmonendra@marvell.com>
2025-01-11 14:10:28 +0530

    octeon: add single rx aura handle per pktio
    This patches fixes single rx aura handle to per pktio interface.
    
    Type: fix
    
    Change-Id: I66aad05382081717767c689c899bf7839570583a
    Signed-off-by: Monendra Singh Kushwaha <kmonendra@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/toolkits/vpp/+/143076
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Nithin Kumar Dabilpuram <ndabilpuram@marvell.com>
    (cherry picked from commit 026373f198502cf15030a67d8c44518871eeb665)
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/toolkits/vpp/+/144310

--- src/plugins/dev_octeon/queue.c
+++ src/plugins/dev_octeon/queue.c
@@ -103,7 +103,7 @@ oct_rxq_init (vlib_main_t *vm, vnet_dev_rx_queue_t *rxq, u32 total_sz)
 
   ASSERT (!(vm->buffer_main->ext_hdr_size % ROC_ALIGN));
 
-  if (!om->use_single_rx_tx_aura || !om->aura_handle)
+  if (!om->use_single_rx_tx_aura || !cd->aura_handle)
     {
       if ((rrv = roc_npa_pool_create (&crq->aura_handle, bp->alloc_size,
 				      total_sz, &aura, &npapool, 0)))
@@ -111,11 +111,11 @@ oct_rxq_init (vlib_main_t *vm, vnet_dev_rx_queue_t *rxq, u32 total_sz)
 	  oct_rxq_deinit (vm, rxq);
 	  return oct_roc_err (dev, rrv, "roc_npa_pool_create() failed");
 	}
-      om->aura_handle = crq->aura_handle;
+      cd->aura_handle = crq->aura_handle;
     }
   else
     {
-      crq->aura_handle = om->aura_handle;
+      crq->aura_handle = cd->aura_handle;
     }
 
   crq->npa_pool_initialized = 1;
